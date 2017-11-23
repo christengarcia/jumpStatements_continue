14287115e2d44e88d8597b3e0352e53994ec5c91
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 22:07:13 +1100

    First commit

---
+++ jumpStatements_continue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace jumpStatements_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("i = {0}", i);
+                if (i == 2)
+                continue; /* the rest of the loop after the continue
+                keyword is skipped for theat iteration.*/
+
+                Console.WriteLine("I will not be printed if i = 2. \n");
+            }
+        }
+    }
+}
